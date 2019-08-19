9e0a2f409e18c620889a1c5eb25c1337723f2f8c
nana1243 <52269210+nana1243@users.noreply.github.com>
2019-08-19 14:18:15 +0900

    새로운것

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+# flask 모듈 임포트
 from flask import Flask, render_template, request
-
+# db 모듈 임포트
 import db
 
+from flask import Blueprint
+from flask_paginate import Pagination, get_page_parameter
+
+
+
+# flask 객체 생성
 app = Flask(__name__)
 
 # 라우터 등록 => 웹상 루트 주소 생성
@@ -9,22 +16,62 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/cu_oneplusone')
-def cu_oneplusone_list():
-    # 데이타베이스 레코드 출력 함수 호출
-    oneplusone_list= db.get_oneplusone_list('df_cu_oneplusone')
+@app.route('/cu_oneplusone/<int:page>', methods=['GET'], defaults={"page": 1})
+def cu_oneplusone_list(page):
+    oneplusone_list= db.get_oneplusone_list('df_cu_oneplusone', page)
     num=len(oneplusone_list)
     data="cu"
-    print(num)
-    return render_template('oneplusone_list_new3.html', oneplusone_list=oneplusone_list, num=num, data="cu")
+    return render_template('oneplusone_list_new4.html', oneplusone_list=oneplusone_list, num=num, data="cu")
+
+@app.route('/cu_oneplusone/<int:page>', methods=['GET'])
+def pagination(page):
+    page = page
+    per_page = 5
+    users = db.sql.paginate(page,per_page,error_out=False)
+    return render_template("oneplusone_list_new4.html", users=users)
+
+
+
+
+
 
 
 @app.route('/mini_oneplusone')
 def ministop_oneplusone_list():
-    data = "ministop"
+    data = ministop
     oneplusone_list= db.get_oneplusone_list('df_ministop_oneplusone')
     num = len(oneplusone_list)
-    return render_template('oneplusone_list_3.html', oneplusone_list=oneplusone_list,num=num, data="ministop")
+    return render_template('oneplusone_list.html', oneplusone_list=oneplusone_list,num=num, data=data)
+
+
+
+@app.route('/twoplusone_list')
+def twoplusone_list():
+    # 데이타베이스 레코드 출력 함수 호출
+    twoplusone_list= db.get_twoplusone_list()
+    # print(country_list)
+    # return 'Success'
+    return render_template('twoplusone_list.html', twoplusone_list=twoplusone_list)
+
+@app.route('/dum_list')
+def dum_list():
+    # 데이타베이스 레코드 출력 함수 호출
+    dum_list= db.get_dum_list()
+    # print(country_list)
+    # return 'Success'
+    return render_template('dum_list.html', dum_list=dum_list)
+
+
+@app.route('/discount_list')
+def discount_list():
+    # 데이타베이스 레코드 출력 함수 호출
+    discount_list= db.get_discount_list()
+    # print(country_list)
+    # return 'Success'
+    return render_template('discount_list.html', discount_list=discount_list)
+
+
+
 
 
 
