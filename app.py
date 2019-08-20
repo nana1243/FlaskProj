8244053fe78e8249a997d7652b33956c9b01ea86
mangokking <53934738+mangokking@users.noreply.github.com>
2019-08-20 17:51:12 +0900

    일단 CU실행용만 올려봐요

--- app.py
+++ app.py
@@ -1,86 +1,205 @@
+# flask 모듈 임포트
 from flask import Flask, render_template, request
-
 import db
 
 from flask import Blueprint
 from flask_paginate import Pagination, get_page_parameter
-
-
-
-# import flask_sqlalchemy
-
+# flask 객체 생성
 app = Flask(__name__)
 
 # 라우터 등록 => 웹상 루트 주소 생성
 @app.route('/')
 def index():
-    return render_template('index.html', active='home')
+    return render_template('index.html')
 
-@app.route('/seven_oneplusone/<int:page>', methods=['GET'])
-def seven_oneplusone_list(page):
-    print(page)
-    data = "seven"
-    oneplusone_list= db.get_oneplusone_list('df_seven_oneplusone', page)
-    num = db.get_oneplusone_list_totCnt('df_seven_oneplusone')
-    pagenum= (num//20)+2
-    return render_template('seven_oneplusone.html', oneplusone_list=oneplusone_list, num=num, pagenum=pagenum, curpage=page, data=data)
+# 메인 검색창
+@app.route('/search_list')
+def search_list():
+    if request.method == 'GET':
+        searchProduct = request.args.get('searchProduct')
+        print(searchProduct)
+        productList = db.get_AllProductList(searchProduct, 'CU')
+        return render_template('allList.html', productList=productList)
+
+# CU 목록표 + 검색창
+@app.route('/cu_oneplusone')
+def cu_oneplusone():
+    productList = db.get_AllProductList('', "CU1")
+    return render_template('cu_oneplusone.html', productList=productList)
+
+@app.route('/search_cu_list1')
+def search_cu_list1():
+    if request.method == 'GET':
+        searchProduct = request.args.get('searchProduct')
+        print(searchProduct)
+        productList = db.get_AllProductList(searchProduct, 'CU1')
+        return render_template('cu_oneplusone.html', productList=productList)
+
+@app.route('/cu_twoplusone')
+def cu_twoplusone():
+    productList = db.get_AllProductList('', "CU2")
+    return render_template('cu_twoplusone.html', productList=productList)
+
+@app.route('/search_cu_list2')
+def search_cu_list2():
+    if request.method == 'GET':
+        searchProduct = request.args.get('searchProduct')
+        print(searchProduct)
+        productList = db.get_AllProductList(searchProduct, 'CU2')
+        return render_template('cu_twoplusone.html', productList=productList)
+
+@app.route('/cu_threeplusone')
+def cu_threeplusone():
+    productList = db.get_AllProductList('', "CU3")
+    return render_template('cu_threeplusone.html', productList=productList)
+
+@app.route('/search_cu_list3')
+def search_cu_list3():
+    if request.method == 'GET':
+        searchProduct = request.args.get('searchProduct')
+        print(searchProduct)
+        productList = db.get_AllProductList(searchProduct, 'CU3')
+        return render_template('cu_threeplusone.html', productList=productList)
+
+@app.route('/cu_dum')
+def cu_dum():
+    productList = db.get_AllProductList('', "CU4")
+    return render_template('cu_dum.html', productList=productList)
+
+@app.route('/search_cu_list4')
+def search_cu_list4():
+    if request.method == 'GET':
+        searchProduct = request.args.get('searchProduct')
+        print(searchProduct)
+        productList = db.get_AllProductList(searchProduct, 'CU4')
+        return render_template('cu_dum.html', productList=productList)
+
+# 7eleven
+@app.route('/seven_oneplusone')
+def seven_oneplusone():
+    productList = db.get_AllProductList('', "seven1")
+    return render_template('seven_oneplusone.html', productList=productList)
+
+@app.route('/search_seven_list1')
+def search_seven_list1():
+    if request.method == 'GET':
+        searchProduct = request.args.get('searchProduct')
+        print(searchProduct)
+        productList = db.get_AllProductList(searchProduct, 'seven1')
+        return render_template('seven_oneplusone.html', productList=productList)
+
+@app.route('/seven_twoplusone')
+def seven_twoplusone():
+    productList = db.get_AllProductList('', "seven2")
+    return render_template('seven_twoplusone.html', productList=productList)
+
+@app.route('/search_seven_list2')
+def search_seven_list2():
+    if request.method == 'GET':
+        searchProduct = request.args.get('searchProduct')
+        print(searchProduct)
+        productList = db.get_AllProductList(searchProduct, 'seven2')
+        return render_template('seven_twoplusone.html', productList=productList)
+
+@app.route('/seven_dum')
+def seven_dum():
+    productList = db.get_AllProductList('', "seven_dum")
+    return render_template('seven_dum.html', productList=productList)
+
+@app.route('/search_seven_list3')
+def search_seven_list3():
+    if request.method == 'GET':
+        searchProduct = request.args.get('searchProduct')
+        print(searchProduct)
+        productList = db.get_AllProductList(searchProduct, 'seven_dum')
+        return render_template('seven_dum.html', productList=productList)
+
+@app.route('/seven_discount')
+def seven_discount():
+    productList = db.get_AllProductList('', "seven_dis")
+    return render_template('seven_discount.html', productList=productList)
+
+@app.route('/search_seven_list4')
+def search_seven_list4():
+    if request.method == 'GET':
+        searchProduct = request.args.get('searchProduct')
+        print(searchProduct)
+        productList = db.get_AllProductList(searchProduct, 'seven_dis')
+        return render_template('seven_discount.html', productList=productList)
+
+# ministop
+@app.route('/ministop_oneplusone')
+def ministop_oneplusone():
+    productList = db.get_AllProductList('', "ministop1")
+    return render_template('ministop_oneplusone.html', productList=productList)
+
+@app.route('/search_ministop_list1')
+def search_ministop_list1():
+    if request.method == 'GET':
+        searchProduct = request.args.get('searchProduct')
+        print(searchProduct)
+        productList = db.get_AllProductList(searchProduct, 'ministop1')
+        return render_template('ministop_oneplusone.html', productList=productList)
+
+@app.route('/ministop_twoplusone')
+def ministop_twoplusone():
+    productList = db.get_AllProductList('', "ministop2")
+    return render_template('ministop_twoplusone.html', productList=productList)
+
+@app.route('/search_ministop_list2')
+def search_ministop_list2():
+    if request.method == 'GET':
+        searchProduct = request.args.get('searchProduct')
+        print(searchProduct)
+        productList = db.get_AllProductList(searchProduct, 'ministop2')
+        return render_template('ministop_twoplusone.html', productList=productList)
+
+@app.route('/ministop_dum')
+def ministop_dum():
+    productList = db.get_AllProductList('', "ministop_dum")
+    return render_template('ministop_dum.html', productList=productList)
+
+@app.route('/search_ministop_list3')
+def search_ministop_list3():
+    if request.method == 'GET':
+        searchProduct = request.args.get('searchProduct')
+        print(searchProduct)
+        productList = db.get_AllProductList(searchProduct, 'ministop_dum')
+        return render_template('ministop_dum.html', productList=productList)
+
+@app.route('/ministop_discount')
+def ministop_discount():
+    productList = db.get_AllProductList('', "ministop_dis")
+    return render_template('ministop_discount.html', productList=productList)
+
+@app.route('/search_ministop_list4')
+def search_ministop_list4():
+    if request.method == 'GET':
+        searchProduct = request.args.get('searchProduct')
+        print(searchProduct)
+        productList = db.get_AllProductList(searchProduct, 'ministop_dis')
+        return render_template('ministop_discount.html', productList=productList)
+# @app.route('/cu_oneplusone/<int:page>', methods=['GET'])
+# def cu_oneplusone_list(page):
+#     print(page)
+#     oneplusone_list= db.get_oneplusone_list('df_cu_oneplusone', page)
+#     num = db.get_oneplusone_list_totCnt('df_cu_oneplusone')
+#     pagenum= (num//20)+2
+#     return render_template('cu.html', oneplusone_list=oneplusone_list, num=num, pagenum=pagenum, curpage=page)
+
+@app.route('/gs25')
+def gs():
+    return render_template('gs25.html')
 
 
+@app.route('/emart')
+def mini():
+    return render_template('emart.html')
 
-
-
-@app.route('/mini_oneplusone')
-def ministop_oneplusone_list():
-    data = "ministop"
-    oneplusone_list= db.get_oneplusone_list('df_ministop_oneplusone')
-    num = len(oneplusone_list)
-    return render_template('oneplusone_list_3.html', oneplusone_list=oneplusone_list,num=num, data="ministop")
-
-
-@app.route('/board')
-def borad_list():
-
-    board_list = db.get_board_list()
-    # print(board_list)
-    # return 'Success'
-    print(board_list)
-    return render_template('board.html', board_list=board_list, active='board')
-
-
-@app.route('/add_board_pro', methods=['post'])
-def add_board_pro():
-    a_title = request.form['a_title']
-    a_content = request.form['a_content']
-    a_writer = request.form['a_writer']
-
-
-    print(a_title, a_writer)
-    # db.py 에 레코드를 추가하는 함수 등록
-    db.addDb_board(a_title, a_content, a_writer)
-
-
-    return render_template('board.html')
-
-@app.route('/board_content', methods=['POST'])
-def board_content() :
-    board_list = db.get_board_list()
-    # print(board_list)
-    # return 'Success'
-    print(board_list)
-    return render_template('board_content.html', board_list=board_list)
-
-
-@app.route('/detailed')
-def detailed():
-    return render_template('board_content.html')
-
-@app.route('/add_board')
-def add_board():
-    return render_template('add_board.html')
-
-
-
+# if __name__ == "__main__" :
+#     app.run()
 
 # 앱 실행  --> 마지막 위치 유지
 # 웹주소와 포트 지정
-app.run(host='127.0.0.1', port=5001, debug=True)
+app.run(host='127.0.0.1', port=1001, debug=True)
+
--- db.py
+++ db.py
@@ -1,21 +1,41 @@
 import pymysql
 
+
 # 데이타베이스 접속 함수
 def get_connection():
     conn = pymysql.connect(
         host='database-1.cb6f1qkai8d4.us-east-2.rds.amazonaws.com',
-        user = 'root',
-        password = '12341234', #자기 my sql 비번
-        db='conv_db', #쓸 데이터베이스이름입력
-        charset='utf8') # 이거 다른지도 체크
+        user='root',
+        password='12341234',  # 자기 my sql 비번
+        db='conv_db',  # 쓸 데이터베이스이름입력
+        charset='utf8')  # 이거 다른지도 체크
 
     return conn
 
+
 # 레코드를 출력하는 함수
+def get_AllProductList(searchProduct, name2):
+    # 데이타베이스 접속함수 호출
+    conn = get_connection()
 
+    # 작업변수 생성
+    cursor = conn.cursor()
 
+    # 쿼리문 생성
+    # sql = "SELECT name, price, event from vw_productlist where name like '%" + searchProduct + "%' order by name asc"
+    # sql = "SELECT * from df_cu_oneplusone where store = " + name2 + "and name like '%" + searchProduct + "%'"
+    sql = " SELECT * from df_all where 편의점 =" + "'" + name2 + "'" + "and 품명 like '%" + searchProduct + "%'"
+    print("----------------------------------------------" + sql)
+    cursor.execute(sql)
 
+    # 리스트 생성
+    productList = cursor.fetchall()
+    print(productList)
 
+    # 데이타베이스 종료
+    conn.close()
+
+    return productList
 
 def get_oneplusone_list(df_name_oneplusone, page):
 
@@ -27,8 +47,7 @@ def get_oneplusone_list(df_name_oneplusone, page):
     print(page)
     page=(int(page)-1)*20
     page = str(page)
-    sql = 'SELECT 품명, 가격, `index` as img FROM ' + df_name_oneplusone + ' LIMIT '+ page + ',20';
-    print(sql)
+    sql = 'SELECT 품명, 가격, `index` as img FROM ' + df_name_oneplusone + ' LIMIT '+ page + ',20'
     cursor.execute(sql)
     oneplusone_list= cursor.fetchall()
     print(oneplusone_list)
@@ -38,6 +57,9 @@ def get_oneplusone_list(df_name_oneplusone, page):
 
     return oneplusone_list
 
+
+
+
 def get_oneplusone_list_totCnt(df_name_oneplusone):
 
     # 데이타베이스 접속함수 호출
@@ -45,7 +67,7 @@ def get_oneplusone_list_totCnt(df_name_oneplusone):
 
     # 작업변수 생성
     cursor = conn.cursor()
-    sql = 'SELECT count(*) FROM ' + df_name_oneplusone ;
+    sql = 'SELECT count(*) FROM ' + df_name_oneplusone
     cursor.execute(sql)
     totCnt= cursor.fetchone()
     print(totCnt[0])
@@ -55,127 +77,25 @@ def get_oneplusone_list_totCnt(df_name_oneplusone):
 
     return int(totCnt[0])
 
-def get_twoplusone_list(df_name_twoplusone):
-
-    # 데이타베이스 접속함수 호출
-    conn = get_connection()
-
-    # 작업변수 생성
-    cursor = conn.cursor()
-
-    # 쿼리문 생성
-    sql = 'SELECT 품명, 가격, `index` as img FROM ' + df_name_twoplusone
-    cursor.execute(sql)
-
-    # 리스트 생성
-    twoplusone_list= cursor.fetchall()
-    #print(oneplusone_list)
-
-    # 데이타베이스 종료
-    conn.close()
-
-    return twoplusone_list
 
 
-
-def get_three_plusone_list(df_name_threeplusone):
-
-    # 데이타베이스 접속함수 호출
-    conn = get_connection()
-
-    # 작업변수 생성
-    cursor = conn.cursor()
-
-    # 쿼리문 생성
-    sql = 'SELECT 품명, 가격, `index` as img FROM ' + df_name_threeplusone
-    cursor.execute(sql)
-
-    # 리스트 생성
-    twoplusone_list= cursor.fetchall()
-    #print(oneplusone_list)
-
-    # 데이타베이스 종료
-    conn.close()
-
-    return twoplusone_list
-
-
-
-def get_dum_list(df_name_dum):
-
-    # 데이타베이스 접속함수 호출
-    conn = get_connection()
-
-    # 작업변수 생성
-    cursor = conn.cursor()
-
-    # 쿼리문 생성
-    sql = 'SELECT 품명, 가격, `index` as img FROM ' + df_name_dum
-
-    cursor.execute(sql)
-
-    # 리스트 생성
-    dum_list= cursor.fetchall()
-
-    # 데이타베이스 종료
-    conn.close()
-
-    return dum_list
-
-
-def get_discount_list(df_name_discount):
-
-    # 데이타베이스 접속함수 호출
-    conn = get_connection()
-
-    # 작업변수 생성
-    cursor = conn.cursor()
-
-    # 쿼리문 생성
-    sql = 'SELECT 품명, 가격, `index` as img FROM ' + df_name_discount
-    cursor.execute(sql)
-
-    # 리스트 생성
-    discount_list= cursor.fetchall()
-
-    # 데이타베이스 종료
-    conn.close()
-
-    return discount_list
-
-
-def get_board_list() :
-
-    conn = get_connection()
-
-    cursor = conn.cursor()
-
-    sql = '''SELECT *
-                FROM flask_db.board ORDER BY id DESC'''
-
-    cursor.execute(sql)
-
-    # 리스트 생성
-    board_list = cursor.fetchall()
-
-    # 데이타베이스 종료
-    conn.close()
-
-    return board_list
-
-
-def addDb_board(a_title, a_content, a_writer):
-    sql = '''
-            insert into flask_db.board
-                (title, content, writer, date, views)
-                values (%s, %s, %s, curdate(), 0)
-            '''
-    # 데이타베이스 접속함수 호출
-    conn = get_connection()
-
-    # 작업변수 생성
-    cursor = conn.cursor()
-    cursor.execute(sql, (a_title, a_writer))
-    conn.commit()
-
-    conn.close()
+# def get_EventProductList(event):
+#     # 데이타베이스 접속함수 호출
+#     conn = get_connection()
+#
+#     # 작업변수 생성
+#     cursor = conn.cursor()
+#
+#     # 쿼리문 생성
+#     # sql = "SELECT name, price, event from vw_productlist where name like '%" + searchProduct + "%' order by name asc"
+#     sql = "SELECT name, price, event from view_all where name like '%" + event + "%' order by event asc"
+#     cursor.execute(sql)
+#
+#     # 리스트 생성
+#     productList2 = cursor.fetchall()
+#     print(productList2)
+#
+#     # 데이타베이스 종료
+#     conn.close()
+#
+#     return productList2
